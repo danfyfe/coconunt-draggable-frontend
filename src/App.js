9486a154ac01507c11d36ed6b303fea402aa9f0a
danfyfe <fyfedaniel@gmail.com>
2019-11-06 18:12:37 -0500

    dans coconut clone for draggable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<>
+    <div className='main-wrapper d-flex flex-column'>
+      <div className='sky'>
+
+        <div className='sun'/>
+
+      </div>
+
+      <div className='water'>
+
+      <div className='d-flex flex-column'>
+
+        <div>
+          <div className='wave'>
+            ~~~~~~
+          </div>
+        </div>
+
+        <div id="fish-div">
+
+        </div>
+
+        <div className='d-flex justify-content-center'>
+          <div className='wave'>
+            ~~~~~~~~~~
+          </div>
+        </div>
+
+        <div className='d-flex justify-content-end'>
+          <div className='wave'>
+            ~~~~~~
+          </div>
+        </div>
+
+      </div>
+
+      </div>
+
+      <div className='sand'>
+
+        <div className='tide'/>
+
+
+        <div className='tree-container'>
+          <img id='coconut-tree' src='https://i.imgur.com/LQlH63o.png' alt='cartoon coconut tree'/>
+          <img id='full-coconut' className='full-coconut' src='https://i.imgur.com/Xv80fwu.png' alt='full coconut'/>
+        </div>
+
+      </div>
+      <div className='more-sand'/>
+
     </div>
-  );
+
+  </>);
 }
 
 export default App;
